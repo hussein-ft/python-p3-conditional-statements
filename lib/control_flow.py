7c22a9a90521f3e39ab12ac59509c2af438b5b62
Hussein <hussein.osman.@student.moringaschool.com>
2025-05-12 09:56:39 +0300

    seve change

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,43 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    """Authenticate user based on username and password."""
+    if username.lower() == "admin" and password == "12345":
+        return "Access granted"
+    return "Access denied"
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    """Return a message based on the temperature."""
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature < 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    """Return Fizz, Buzz, FizzBuzz, or the number based on divisibility."""
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    """Perform basic arithmetic operations."""
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        print("Invalid operation!")
+        return None
